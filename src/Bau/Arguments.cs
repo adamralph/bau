8496b9604369be060b3ef89ceed00a724dbddf0a
Adam Ralph <adam@adamralph.com>
2013-12-01 20:25:15 +0000

    extracted BaufileFinder and ApplicationFactory from Program

--- src/Bau/Arguments.cs
+++ src/Bau/Arguments.cs
@@ -4,12 +4,16 @@
 
 namespace Bau
 {
+    using System;
     using System.Collections.Generic;
     using CommandLine;
     using CommandLine.Text;
 
     public class Arguments
     {
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
         [ValueList(typeof(List<string>))]
         public IList<string> TargetNames { get; set; }
 
@@ -24,7 +28,17 @@ namespace Bau
         {
             var help = new HelpText();
             help.Heading = "Usage: bau {options} targets...";
-            help.AddPreOptionsLine(" ");
+
+            if (this.LastParserState != null && this.LastParserState.Errors.Count > 0)
+            {
+                var errors = help.RenderParsingErrorsText(this, 2); // indent with two spaces
+                if (!string.IsNullOrEmpty(errors))
+                {
+                    help.AddPreOptionsLine(string.Concat(Environment.NewLine, "ERROR(S):"));
+                    help.AddPreOptionsLine(errors);
+                }
+            }
+
             help.AddPreOptionsLine("Options are...");
             help.AddDashesToOption = true;
             help.AddOptions(this);
--- src/Bau/Program.cs
+++ src/Bau/Program.cs
@@ -6,14 +6,10 @@ namespace Bau
 {
     using System;
     using System.Globalization;
-    using System.IO;
     using System.Linq;
-    using Bau.Scripting;
     using CommandLine;
     using Common.Logging;
     using Common.Logging.Simple;
-    using ScriptCs;
-    using ScriptCs.Contracts;
     using ServiceStack.Text;
 
     internal static class Program
@@ -24,6 +20,14 @@ namespace Bau
 
             LogManager.Adapter = new ConsoleOutLoggerFactoryAdapter(Common.Logging.LogLevel.Trace, true, true, true, "u");
             var log = LogManager.GetCurrentClassLogger();
+
+            AppDomain.CurrentDomain.FirstChanceException +=
+                (object sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e) =>
+                log.Trace("First chance exception.", e.Exception);
+
+            AppDomain.CurrentDomain.UnhandledException += (object sender, UnhandledExceptionEventArgs e) =>
+                log.Fatal("Unhandled exception.", (Exception)e.ExceptionObject);
+
             log.TraceFormat(CultureInfo.InvariantCulture, "Arguments: {0}", args.ToJsv());
 
             var arguments = new Arguments();
@@ -34,46 +38,9 @@ namespace Bau
 
             log.TraceFormat(CultureInfo.InvariantCulture, "Parsed arguments: {0}", arguments.ToJsv());
 
-            // TODO (adamralph): 
-            ////'rakefile',
-            ////'Rakefile',
-            ////'rakefile.rb',
-            ////'Rakefile.rb'
-            var filename = "baufile.csx";
-            while (!File.Exists(filename))
-            {
-                var directory = Directory.GetCurrentDirectory();
-                log.TraceFormat(CultureInfo.InvariantCulture, "No Baufile found in directory '{0}'.", directory);
-
-                var parent = Directory.GetParent(directory);
-                if (parent == null)
-                {
-                    throw new InvalidOperationException("No Baufile found (looking for baufile.csx).");
-                }
-
-                Directory.SetCurrentDirectory(parent.FullName);
-            }
-
-            log.TraceFormat(CultureInfo.InvariantCulture, "The current directory is '{0}'.", Directory.GetCurrentDirectory());
-            log.DebugFormat(CultureInfo.InvariantCulture, "Executing '{0}'...", Path.GetFullPath(filename));
-
-            var application = new Application();
-
-            // TODO (adamralph): move out for easily swappable scripting solutions
-            var fileSystem = new FileSystem { CurrentDirectory = Directory.GetCurrentDirectory() };
-            using (var executor = new BauScriptExecutor(application, fileSystem))
-            {
-                executor.AddReferenceAndImportNamespaces(new[] { typeof(Target) });
-                executor.Initialize(new string[0], new IScriptPack[0]);
-                executor.Execute(filename);
-            }
-
-            if (arguments.TargetNames.Count == 0)
-            {
-                arguments.TargetNames.Add("default");
-            }
-
-            foreach (var target in arguments.TargetNames.Select(name => application.GetTarget(name)))
+            var application = ApplicationFactory.Create(BaufileFinder.Find());
+            foreach (var target in (arguments.TargetNames.Count == 0 ? new[] { "default" } : arguments.TargetNames)
+                .Select(name => application.GetTarget(name)))
             {
                 target.Invoke(application);
             }
