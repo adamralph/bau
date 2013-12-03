5263aa05e08b4c2ab152b415dbd5083de6f3dd86
Adam Ralph <adam@adamralph.com>
2013-12-03 22:06:06 +0000

    added skeleton MSBuild target

---
+++ src/Bau/MSBuild.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="MSBuild.cs" company="Bau contributors">
+//  Copyright (c) Bau contributors. (baubuildch@gmail.com)
+// </copyright>
+
+namespace Bau
+{
+    using System.Collections.Generic;
+
+    public class MSBuild : Exec
+    {
+        public string Solution { get; set; }
+
+        public string Verbosity { get; set; }
+
+        public string Logger { get; set; }
+
+        public int MaxCpuCount { get; set; }
+
+        public string[] Targets { get; set; }
+
+        public Dictionary<string, string> Properties { get; set; }
+
+        public Dictionary<string, string> OtherSwitches { get; set; }
+    }
+}
