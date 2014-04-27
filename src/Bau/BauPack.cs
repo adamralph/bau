0db4fad55b04d55b321663b8afbcef17605fd8d5
Adam Ralph <adam@adamralph.com>
2014-04-27 23:16:01 +0200

    #11 green

---
+++ src/Bau/BauPack.cs
@@ -0,0 +1,116 @@
+﻿// <copyright file="BauPack.cs" company="Bau contributors">
+//  Copyright (c) Bau contributors. (baubuildch@gmail.com)
+// </copyright>
+
+namespace Bau
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Reflection;
+    using ScriptCs.Contracts;
+
+    public class BauPack : IScriptPackContext
+    {
+        private readonly List<string> topLevelTaskNames = new List<string>();
+        private readonly Dictionary<string, Task> tasks = new Dictionary<string, Task>();
+
+        public BauPack(IEnumerable<string> topLevelTaskNames)
+        {
+            if (topLevelTaskNames != null)
+            {
+                this.topLevelTaskNames.AddRange(topLevelTaskNames);
+            }
+
+            if (this.topLevelTaskNames.Count == 0)
+            {
+                this.topLevelTaskNames.Add("default");
+            }
+        }
+
+        public IEnumerable<Task> Tasks
+        {
+            get { return this.tasks.Select(t => t.Value); }
+        }
+
+        public IEnumerable<string> TopLevelTaskNames
+        {
+            get { return this.topLevelTaskNames.Select(n => n); }
+        }
+
+        public void Task(string name, Action action)
+        {
+            this.Task(name, (Task task) => action());
+        }
+
+        public void Task<ITask>(string name, Action<ITask> action)
+            where ITask : Task, new()
+        {
+            var task = this.Intern<ITask>(name);
+            ////if (prerequisites != null)
+            ////{
+            ////    foreach (var prerequisite in prerequisites.Where(p => !task.Prerequisites.Contains(p)))
+            ////    {
+            ////        task.Prerequisites.Add(prerequisite);
+            ////    }
+            ////}
+
+            if (action != null)
+            {
+                task.Actions.Add(() => action(task));
+            }
+        }
+
+        public void Execute()
+        {
+            var version = (AssemblyInformationalVersionAttribute)Assembly.GetExecutingAssembly()
+                .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute)).Single();
+
+            Console.WriteLine("Bau version {0}.", version.InformationalVersion);
+            Console.WriteLine("Copyright (c) Bau contributors. (baubuildch@gmail.com)");
+
+            foreach (var task in this.TopLevelTaskNames.Select(name => this.GetTask(name)))
+            {
+                task.Invoke(this);
+            }
+
+            Console.WriteLine("Bau succeeded.");
+        }
+
+        public Task GetTask(string name)
+        {
+            Task task;
+            if (!this.tasks.TryGetValue(name, out task))
+            {
+                var message = string.Format(CultureInfo.InvariantCulture, "'{0}' task not found.", name);
+                throw new InvalidOperationException(message);
+            }
+
+            return task;
+        }
+
+        private TTask Intern<TTask>(string name) where TTask : Task, new()
+        {
+            Task task;
+            if (!this.tasks.TryGetValue(name, out task))
+            {
+                this.tasks.Add(name, task = new TTask() { Name = name });
+            }
+
+            var typedTask = task as TTask;
+            if (typedTask == null)
+            {
+                var message = string.Format(
+                    CultureInfo.InvariantCulture,
+                    "'{0}' task already exists with type '{1}'.",
+                    name,
+                    task.GetType().Name);
+
+                throw new InvalidOperationException(message);
+            }
+
+            return typedTask;
+        }
+    }
+}
---
+++ src/Bau/Task.cs
@@ -0,0 +1,111 @@
+﻿// <copyright file="Task.cs" company="Bau contributors">
+//  Copyright (c) Bau contributors. (baubuildch@gmail.com)
+// </copyright>
+
+namespace Bau
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    public class Task
+    {
+        private readonly List<string> prerequisites = new List<string>();
+        private readonly List<Action> actions = new List<Action>();
+
+        private string name;
+        private string description;
+        private bool alreadyInvoked;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    var message = string.Format(CultureInfo.InvariantCulture, "Invalid task name '{0}'.", value);
+                    throw new ArgumentException(message, "value");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return this.description;
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid description.", "value");
+                }
+
+                this.description = value;
+            }
+        }
+
+        public IList<string> Prerequisites
+        {
+            get { return this.prerequisites; }
+        }
+
+        public IList<Action> Actions
+        {
+            get { return this.actions; }
+        }
+
+        public virtual void Invoke(BauPack application)
+        {
+            Guard.AgainstNullArgument("application", application);
+
+            var trace = this.alreadyInvoked ? null : " (first time)";
+            ////log.TraceFormat(CultureInfo.InvariantCulture, "Invoking '{0}'{1}.", this.Name, trace);
+            if (this.alreadyInvoked)
+            {
+                ////log.TraceFormat(CultureInfo.InvariantCulture, "Already invoked '{0}'. Ignoring invocation.", this.Name);
+                return;
+            }
+
+            this.alreadyInvoked = true;
+            foreach (var prerequisite in this.Prerequisites.Select(name => application.GetTask(name)))
+            {
+                prerequisite.Invoke(application);
+            }
+
+            try
+            {
+                this.Execute();
+            }
+            catch (Exception ex)
+            {
+                var message = string.Format(CultureInfo.InvariantCulture, "'{0}' task failed. {1}", this.name, ex.Message);
+                throw new InvalidOperationException(message, ex);
+            }
+        }
+
+        public virtual void Execute()
+        {
+            if (this.actions.Count == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine("Executing '{0}' Bau task.", this.Name);
+            foreach (var action in this.actions)
+            {
+                action();
+            }
+        }
+    }
+}
