2cccc703eeafdc45bbfb43cfdc9d1c65a9cac0c6
Adam Ralph <adam@adamralph.com>
2014-04-30 08:32:42 +0200

    #23 green: added TaskDependencies.MultipleDependencies

--- src/test/Bau.Test.Acceptance/TaskDependencies.cs
+++ src/test/Bau.Test.Acceptance/TaskDependencies.cs
@@ -81,5 +81,85 @@ bau.Execute();"));
             "And I am informed that the default task was executed"
                 .f(() => output.Should().Contain("Executing 'default' Bau task."));
         }
+
+        [SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "Disposed by xBehave.net.")]
+        [Scenario]
+        public static void MultipleDependencies(Baufile baufile, string tempFile, string[] excutedTasks, string output)
+        {
+            var scenario = MethodInfo.GetCurrentMethod().GetFullName();
+
+            "Given bau is required"
+                .f(() => baufile = Baufile.Create(scenario).WriteLine(
+@"var bau = Require<BauPack>();"));
+
+            "And a non-default task"
+                .f(c => baufile.WriteLine(
+@"
+var executed = new List<string>();
+
+bau
+    .Task(""non-default1"")
+    .Do(() => executed.Add(""non-default1""));"));
+
+            "And a second non-default task"
+                .f(c => baufile.WriteLine(
+@"
+bau
+    .Task(""non-default2"")
+    .Do(() => executed.Add(""non-default2""));"));
+
+            "And a default task which depends on both non-default tasks"
+                .f(() =>
+                {
+                    tempFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N", CultureInfo.InvariantCulture));
+                    baufile.WriteLine(
+    @"
+bau
+    .Task(""default"")
+    .DependsOn(""non-default1"", ""non-default2"")
+    .Do(() =>
+    {
+        executed.Add(""default"");
+        using(var file = File.CreateText(@""" + tempFile + @"""))
+        {
+            file.Write(string.Join(Environment.NewLine, executed));
+        };
+    });");
+                });
+
+            "And the tasks are executed"
+                .f(() => baufile.WriteLine(
+@"
+bau.Execute();"));
+
+            "When I execute the baufile"
+                .f(() => output = baufile.Execute());
+
+            "Then three tasks are executed"
+                .f(() =>
+                {
+                    File.Exists(tempFile).Should().BeTrue();
+                    excutedTasks = File.ReadAllText(tempFile).Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                    excutedTasks.Length.Should().Be(3);
+                });
+
+            "And the first non-default task is executed first"
+                .f(() => excutedTasks[0].Should().Be("non-default1"));
+
+            "And the second non-default task is executed second"
+                .f(() => excutedTasks[1].Should().Be("non-default2"));
+
+            "And the default task is executed third"
+                .f(() => excutedTasks[2].Should().Be("default"));
+
+            "And I am informed that the first non-default task was executed"
+                .f(() => output.Should().Contain("Executing 'non-default1' Bau task."));
+
+            "And I am informed that the second non-default task was executed"
+                .f(() => output.Should().Contain("Executing 'non-default2' Bau task."));
+
+            "And I am informed that the default task was executed"
+                .f(() => output.Should().Contain("Executing 'default' Bau task."));
+        }
     }
 }
