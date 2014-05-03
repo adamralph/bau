e48e1cd30bf2016ee2407bf4de2386d670f7bf64
Adam Ralph <adam@adamralph.com>
2014-05-03 22:00:30 +0200

    #49 refactor: add HelloWorld sample plugin

---
+++ src/samples/plugins/Bau.HelloWorld/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="HelloWorld.cs" company="Bau contributors">
+//  Copyright (c) Bau contributors. (baubuildch@gmail.com)
+// </copyright>
+
+// Require<BauPack>().Task<HelloWorld>().Execute();
+public class HelloWorld : Bau.Task
+{
+    protected override void OnActionsExecuted()
+    {
+        System.Console.WriteLine("Hello world!");
+    }
+}
