773d71b9547fa5fa1d07da5144221420bbe44f39
Adam Ralph <adam@adamralph.com>
2014-05-03 21:43:40 +0200

    #49 refactor: simplify greeter sample plugin

---
+++ src/samples/Bau.Samples.Greeter/Greeter.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="Greeter.cs" company="Bau contributors">
+//  Copyright (c) Bau contributors. (baubuildch@gmail.com)
+// </copyright>
+
+// Require<BauPack>().Greeter().Do(g => g.Greeting = "Hello world!").Execute();
+namespace BauGreeter
+{
+    using Bau;
+
+    public class Greeter : Task
+    {
+        public string Greeting { get; set; }
+
+        protected override void OnActionsExecuted()
+        {
+            System.Console.WriteLine(this.Greeting);
+        }
+    }
+
+    public static class Plugin
+    {
+        public static IBauPack<Greeter> Greeter(this IBauPack bau, string name = BauPack.DefaultTask)
+        {
+            return new BauPack<Greeter>(bau, name);
+        }
+    }
+}
