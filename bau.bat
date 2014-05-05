4077a0c04a681c9e37f1035e6bf7c56df6cb65c1
Adam Ralph <adam@adamralph.com>
2014-05-05 18:35:43 +0200

    refactor: update to 0.1.0-alpha07

--- bau.bat
+++ bau.bat
@@ -4,4 +4,4 @@ REM this can be removed when https://github.com/scriptcs/scriptcs/issues/297 is 
 scriptcs -install -pre
 
 REM after packages have been installed, you can optionally use the following command in your console instead of running this batch file
-scriptcs baufile.csx
+scriptcs baufile.csx -- %1
