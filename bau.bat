fd0dd08987daa2ca4de854b0ba97b77d87fcb2a3
Adam Ralph <adam@adamralph.com>
2014-05-05 18:50:56 +0200

    refactor: fix bau.bat to pass multiple args

--- bau.bat
+++ bau.bat
@@ -1,7 +1,7 @@
-@echo off
+@ECHO OFF
 
 REM this can be removed when https://github.com/scriptcs/scriptcs/issues/297 is fixed
 scriptcs -install -pre
 
 REM after packages have been installed, you can optionally use the following command in your console instead of running this batch file
-scriptcs baufile.csx -- %1
+scriptcs baufile.csx -- %*
