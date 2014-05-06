77560978fbedc4a66d1cdd41a9112c6655840060
Adam Ralph <adam@adamralph.com>
2014-05-06 07:18:23 +0200

    #11 red: added TaskExecution.ExecutingAnInlineTask

--- src/test/Bau.Test.Acceptance/TaskExecution.cs
+++ src/test/Bau.Test.Acceptance/TaskExecution.cs
@@ -58,5 +58,36 @@ namespace Bau.Test.Acceptance
             "And I am informed that the default task was not found"
                 .f(() => ex.Message.Should().Contain("'default' task not found"));
         }
+
+        [Scenario]
+        public static void ExecutingAnInlineTask(Baufile baufile, string tempFile, string output)
+        {
+            var scenario = MethodInfo.GetCurrentMethod().GetFullName();
+
+            "Given a baufile with a default task with an inline task"
+                .f(() =>
+                {
+                    tempFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N", CultureInfo.InvariantCulture));
+                    baufile = Baufile.Create(scenario).WriteLine(
+@"Require<Bau>()
+.Do(() =>
+{
+    var task = new BauCore.Task();
+    task.Actions.Add(() => File.Create(@""" + tempFile + @""").Dispose());
+    task.Execute();
+})
+.Execute();");
+                })
+                .Teardown(() => File.Delete(tempFile));
+
+            "When I execute the baufile"
+                .f(() => output = baufile.Execute());
+
+            "Then the task is executed"
+                .f(() => File.Exists(tempFile).Should().BeTrue());
+
+            "And I should not be informed that a task with no name was executed"
+                .f(() => output.Should().NotContain("Executing '' Bau task."));
+        }
     }
 }
