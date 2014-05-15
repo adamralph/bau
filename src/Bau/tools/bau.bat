af7f9655bb530901e79e93a54f04868f142e56c5
twiernik <twiernik@gmail.com>
2014-05-15 20:46:03 +0200

    Sample .bat file which wraps execution of baufile.csx

---
+++ src/Bau/tools/bau.bat
@@ -0,0 +1,9 @@
+REM TIP: copy this file to your PATH so you can type 'bau' next to any baufile.csx
+
+@ECHO OFF
+
+REM this can be removed when https://github.com/scriptcs/scriptcs/issues/297 is fixed
+scriptcs -install
+
+REM after packages have been installed, you can optionally use the following command in your console instead of running this batch file
+scriptcs baufile.csx -- %*
