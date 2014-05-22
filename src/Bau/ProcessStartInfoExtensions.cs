bac75a39d8b3d75c617cc91eae8f3436416fcc61
Adam Ralph <adam@adamralph.com>
2014-05-22 20:50:30 +0200

    #142 green: added extension method for process execution

---
+++ src/Bau/ProcessStartInfoExtensions.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="ProcessStartInfoExtensions.cs" company="Bau contributors">
+//  Copyright (c) Bau contributors. (baubuildch@gmail.com)
+// </copyright>
+
+namespace BauCore
+{
+    using System;
+    using System.Diagnostics;
+    using System.Globalization;
+
+    public static class ProcessStartInfoExtensions
+    {
+        public static void Run(this ProcessStartInfo info)
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo = info;
+                process.Start();
+                process.WaitForExit();
+                if (process.ExitCode != 0)
+                {
+                    var message = string.Format(
+                        CultureInfo.InvariantCulture,
+                        "The command exited with code {0}.",
+                        process.ExitCode.ToString(CultureInfo.InvariantCulture));
+
+                    throw new InvalidOperationException(message);
+                }
+            }
+        }
+    }
+}
