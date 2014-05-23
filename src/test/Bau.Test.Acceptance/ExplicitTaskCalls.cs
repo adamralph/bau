41a3e10e3aa3593b828bfc05138007e12cacd9aa
pgermishuys <pieterg@D101980.SAHL.com>
2014-05-23 15:10:25 +0200

    Renamed copyright file reference

--- src/test/Bau.Test.Acceptance/ExplicitTaskCalls.cs
+++ src/test/Bau.Test.Acceptance/ExplicitTaskCalls.cs
@@ -1,28 +1,28 @@
-﻿// <copyright file="ExecuteATaskFromATask.cs" company="Bau contributors">
+﻿// <copyright file="ExplicitTaskCalls.cs" company="Bau contributors">
 //  Copyright (c) Bau contributors. (baubuildch@gmail.com)
 // </copyright>
 
 namespace Bau.Test.Acceptance
 {
-	using System;
-	using System.Globalization;
-	using System.IO;
-	using System.Reflection;
-	using Bau.Test.Acceptance.Support;
-	using FluentAssertions;
-	using Xbehave;
-	using Xunit;
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Reflection;
+    using Bau.Test.Acceptance.Support;
+    using FluentAssertions;
+    using Xbehave;
+    using Xunit;
 
-	public static class ExplicitTaskCalls
-	{
-		// happy path
-		[Scenario]
-		public static void ExplicitTaskExecution(Baufile baufile, string tempFile, string[] executedTasks, string output)
-		{
-			var scenario = MethodBase.GetCurrentMethod().GetFullName();
-			tempFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N", CultureInfo.InvariantCulture));
-			"Given an ExplicitTaskExecution baufile"
-				.f(() => baufile = Baufile.Create(scenario).WriteLine(
+    public static class ExplicitTaskCalls
+    {
+        // happy path
+        [Scenario]
+        public static void ExplicitTaskExecution(Baufile baufile, string tempFile, string[] executedTasks, string output)
+        {
+            var scenario = MethodBase.GetCurrentMethod().GetFullName();
+            tempFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N", CultureInfo.InvariantCulture));
+            "Given an ExplicitTaskExecution baufile"
+                .f(() => baufile = Baufile.Create(scenario).WriteLine(
 @"var executed = new List<string>();
 var bau = Require<Bau>();
 bau.Task(""non-default"")
@@ -38,39 +38,39 @@ bau.Task(""non-default"")
     };
 })
 .Run();"));
-			"When I execute the baufile"
-				.f(() => output = baufile.Run());
+            "When I execute the baufile"
+                .f(() => output = baufile.Run());
 
-			"Then two tasks are executed"
-				.f(() =>
-				{
-					File.Exists(tempFile).Should().BeTrue();
-					executedTasks = File.ReadAllText(tempFile).Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-					executedTasks.Length.Should().Be(2);
-				});
+            "Then two tasks are executed"
+                .f(() =>
+                {
+                    File.Exists(tempFile).Should().BeTrue();
+                    executedTasks = File.ReadAllText(tempFile).Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                    executedTasks.Length.Should().Be(2);
+                });
 
-			"And the non-default task is executed first"
-				.f(() => executedTasks[0].Should().Be("non-default"));
+            "And the non-default task is executed first"
+                .f(() => executedTasks[0].Should().Be("non-default"));
 
-			"And the default task is executed second"
-				.f(() => executedTasks[1].Should().Be("default"));
+            "And the default task is executed second"
+                .f(() => executedTasks[1].Should().Be("default"));
 
-			"And I am informed that the non-default task was executed"
-				.f(() => output.Should().ContainEquivalentOf("starting 'non-default'"));
+            "And I am informed that the non-default task was executed"
+                .f(() => output.Should().ContainEquivalentOf("starting 'non-default'"));
 
-			"And I am informed that the default task was executed"
-				.f(() => output.Should().ContainEquivalentOf("starting 'default'"));
-		}
+            "And I am informed that the default task was executed"
+                .f(() => output.Should().ContainEquivalentOf("starting 'default'"));
+        }
 
-		[Scenario]
-		public static void ExplicitTaskInvocation(Baufile baufile, string tempFile, string[] executedTasks, string output)
-		{
-			var scenario = MethodBase.GetCurrentMethod().GetFullName();
-			tempFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N", CultureInfo.InvariantCulture));
+        [Scenario]
+        public static void ExplicitTaskInvocation(Baufile baufile, string tempFile, string[] executedTasks, string output)
+        {
+            var scenario = MethodBase.GetCurrentMethod().GetFullName();
+            tempFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N", CultureInfo.InvariantCulture));
 
-			"Given an ExplicitTaskInvocation baufile"
-				.f(() => baufile = Baufile.Create(scenario).WriteLine(
-				@"var executed = new List<string>();
+            "Given an ExplicitTaskInvocation baufile"
+                .f(() => baufile = Baufile.Create(scenario).WriteLine(
+                @"var executed = new List<string>();
 var bau = Require<Bau>();
 bau.Task(""parent"")
 .DependsOn(""child"")
@@ -88,34 +88,34 @@ bau.Task(""parent"")
 	};
 })
 .Run();"));
-			"When I execute the baufile"
-				.f(() => output = baufile.Run());
+            "When I execute the baufile"
+                .f(() => output = baufile.Run());
 
-			"Then three tasks are executed"
-				.f(() =>
-				{
-					File.Exists(tempFile).Should().BeTrue();
-					executedTasks = File.ReadAllText(tempFile).Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-					executedTasks.Length.Should().Be(3);
-				});
+            "Then three tasks are executed"
+                .f(() =>
+                {
+                    File.Exists(tempFile).Should().BeTrue();
+                    executedTasks = File.ReadAllText(tempFile).Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                    executedTasks.Length.Should().Be(3);
+                });
 
-			"And the child task is executed first"
-				.f(() => executedTasks[0].Should().Be("child"));
+            "And the child task is executed first"
+                .f(() => executedTasks[0].Should().Be("child"));
 
-			"And the parent task is executed second"
-				.f(() => executedTasks[1].Should().Be("parent"));
+            "And the parent task is executed second"
+                .f(() => executedTasks[1].Should().Be("parent"));
 
-			"And the default task is executed third"
-				.f(() => executedTasks[2].Should().Be("default"));
+            "And the default task is executed third"
+                .f(() => executedTasks[2].Should().Be("default"));
 
-			"And I am informed that the child task was executed"
-			   .f(() => output.Should().ContainEquivalentOf("starting 'child'"));
+            "And I am informed that the child task was executed"
+               .f(() => output.Should().ContainEquivalentOf("starting 'child'"));
 
-			"And I am informed that the parent task was executed"
-				.f(() => output.Should().ContainEquivalentOf("starting 'parent'"));
+            "And I am informed that the parent task was executed"
+                .f(() => output.Should().ContainEquivalentOf("starting 'parent'"));
 
-			"And I am informed that the default task was executed"
-				.f(() => output.Should().ContainEquivalentOf("starting 'default'"));
-		}
-	}
+            "And I am informed that the default task was executed"
+                .f(() => output.Should().ContainEquivalentOf("starting 'default'"));
+        }
+    }
 }
