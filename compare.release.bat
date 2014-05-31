c58b3ddc64e91d9411b6471feac0ee0db1bf0717
Adam Ralph <adam@adamralph.com>
2014-05-31 15:20:31 +0200

    refactor: upgrade build packages

--- compare.release.bat
+++ compare.release.bat
@@ -1,2 +1,2 @@
-xcopy src\Bau\bin\Release\Bau.dll comparison\packages\Bau.0.1.0-alpha14\lib\net45\ /Y /Q
-xcopy src\Bau.Exec\bin\Release\Bau.Exec.dll comparison\packages\Bau.Exec.0.1.0-alpha14\lib\net45\ /Y /Q
+xcopy src\Bau\bin\Release\Bau.dll comparison\packages\Bau.0.1.0-alpha15\lib\net45\ /Y /Q
+xcopy src\Bau.Exec\bin\Release\Bau.Exec.dll comparison\packages\Bau.Exec.0.1.0-alpha15\lib\net45\ /Y /Q
